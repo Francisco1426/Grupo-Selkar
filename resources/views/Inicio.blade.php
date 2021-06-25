a6cc9a894075965968612dc9c9dd9e68b9943a6b
Eduardo Garcia <al221811274@gmail.com>
2021-06-25 12:58:59 -0500

    Prueba Eduardo

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -9,7 +9,7 @@
 		<div class="clearfix"></div>
 		<footer>
 			<div class="container-fluid">
-				<p class="copyright">&copy; 2021 <a href="https://www.themeineed.com" target="_blank">UTVT</a>. All Rights Reserved.</p>
+				<p class="copyright">&copy; 2021 <a href="https://www.themeineed.com" target="_blank">UTVT</a>. Selkar.</p>
 			</div>
 		</footer>
 	</div>
