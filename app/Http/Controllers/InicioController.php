ba1f40502dab833ef06bad95de2f44271dcce5b0
Omar Chong <al221811717@gmail.com>
2021-06-29 10:47:21 -0500

    Instalacion de libreria de roles y permisos

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -6,6 +6,17 @@ use Illuminate\Http\Request;
 
 class InicioController extends Controller
 {
+    public function resgistrausuario(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $user->asignarRol('cliente');
+    }
+
     public function Inicio()
     {
         return view('Inicio');
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\http\Controllers\InicioController;
 
 Route::get('Inicio', [InicioController::class,'Inicio'])->name('Inicio');
+Route::group(['middleware' => ['role:cliente']], function () {
+    //rutas accesibles solo para clientes
+});
